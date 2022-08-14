c33445322dcecf20dbed73368dc5b83aca00b90a
Mostafaemad228 <42499279+Mostafaemad228@users.noreply.github.com>
2022-08-14 05:01:44 +0200

    commit

---
+++ js/custom.js
@@ -0,0 +1,71 @@
+ $(window).on('load', function () {
+    $('#preloader-active').delay(450).fadeOut('slow');
+    $('body').delay(850).css({
+      'overflow': 'visible'
+    });
+  });
+  // /////////////////////////////////////////
+  window.addEventListener("scroll" , function () {    
+    var navbar =  document.querySelector("nav");
+    navbar.classList.toggle("sticky",window.scrollY >100);
+  })
+  // ///////////////////////////////////////////////////////////// ///////////////////////
+ /* 1. Proloder */
+// $(window).on('load', function () {
+//     $('#preloader-Div').delay(450).fadeOut('slow');
+//     $('body').delay(850).css({
+//     'overflow': 'visible'
+//     });
+// });
+// // ////////////////////////// /////////////////////// ///////////////////////
+var typed = new Typed(".type-text", {
+  strings: [
+    "ceo devfolio",
+    "_Get Your Degree Now.",
+    "_Be a part of a professional community.",
+    "_traning for feuture.",],
+  typeSpeed: 100,
+  backSpeed: 50,
+  // smartBackspace: true,
+  loop: false,
+  loopCount: Infinity,
+});
+
+// ///////////////////////////////////////////////////////////////////////////
+
+  
+  
+
+
+  
+  
+     
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
